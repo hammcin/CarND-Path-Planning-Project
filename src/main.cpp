823dc875aafb5de4acdcf9e8519c7ad69e48d0ec
Hamadi McIntosh <hamadi.mc@sbcglobal.net>
2019-06-11 10:52:51 -0400

    fix: Drive fast and create spline trajectories

--- src/main.cpp
+++ src/main.cpp
@@ -57,8 +57,8 @@ int main() {
   // Have a reference velocity to target
   double ref_vel = 0.0; // mph
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  h.onMessage([&ref_vel,&lane,&map_waypoints_x,&map_waypoints_y,
+               &map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
